7576bda16f777d3806e21bdb8c1f17b1d011ea85
Mike Robinson <mike@securebroadcast.com>
2018-06-04 14:48:33 +0100

    Created xcode project and added some dev tools

---
+++ LearnToiOS-Lesson4/LearnToiOS-Lesson4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LearnToiOS-Lesson4
+//
+//  Created by Mike Robinson on 04/06/2018.
+//  Copyright © 2018 Mike Robinson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
