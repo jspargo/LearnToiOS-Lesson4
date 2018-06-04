8c07fde68dbcdd4b6687f45277e442f8d84d7197
Mike Robinson <mike@securebroadcast.com>
2018-06-04 23:30:29 +0100

    Added pinch to zoom and rotation gestures Allowed them to be recognised simultaneously Added long press gesture to move

--- LearnToiOS-Lesson4/LearnToiOS-Lesson4/AvatarViewController.swift
+++ LearnToiOS-Lesson4/LearnToiOS-Lesson4/AvatarViewController.swift
@@ -35,4 +35,47 @@ class AvatarViewController: UIViewController {
     self.dismiss(animated: true) { }
     
   }
+  
+  @IBAction func didRecognisePinch(_ sender: UIPinchGestureRecognizer) {
+    NSLog("\(#function): \(sender.scale)")
+    
+    let newTransform = previousTransform.scaledBy(x: sender.scale, y: sender.scale)
+
+    self.avatarImageView.transform = newTransform
+
+    previousTransform = newTransform
+    
+    sender.scale = 1
+
+  }
+  
+  
+  @IBAction func didRecogniseRotation(_ sender: UIRotationGestureRecognizer) {
+    NSLog("\(#function): \(sender.rotation)")
+
+    let newRotation = previousTransform.rotated(by: sender.rotation)
+    self.avatarImageView.transform = newRotation
+    
+    previousTransform = newRotation
+    
+    sender.rotation = 0
+
+  }
+  
+  @IBAction func didRecogniseLongPress(_ sender: UILongPressGestureRecognizer) {
+    NSLog("\(#function)")
+    
+    let newCenter = sender.location(ofTouch: 0, in: self.view)
+    
+    self.avatarImageView.center = newCenter
+
+  }
+  
+}
+
+extension AvatarViewController: UIGestureRecognizerDelegate {
+  
+  func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+    return true
+  }
 }
